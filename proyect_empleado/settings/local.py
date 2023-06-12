c8c7994e972fc3f89ef6a46db044864a38d8019a
Enmanuel77 <135169042+Enmanuel77@users.noreply.github.com>
2023-06-12 13:45:38 -0600

    Update local.py

--- proyect_empleado/settings/local.py
+++ proyect_empleado/settings/local.py
@@ -20,7 +20,6 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATICFILES_DIRS = [BASE_DIR.child('static')]
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
